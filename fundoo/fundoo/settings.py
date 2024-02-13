0eadc671d96d584e9eb71c7675effdcc7d63cf7d
Gurmehar Kaur <78135405+gurmehar1553@users.noreply.github.com>
2024-02-13 19:05:27 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -103,10 +103,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chatapp',
-        'USER': 'chatapp_user',
-        'PASSWORD': 'chatapp@123',
-        'HOST': '10.0.12.119',
+        'NAME': os.environ.get('CHATDB'),
+        'USER': os.environ.get('CHATDBUSER'),
+        'PASSWORD': os.environ.get('CHATDBPASSWORD'),
+        'HOST': os.environ.get('CHATDBHOST'),
         'PORT': '3306',
     }
 }
